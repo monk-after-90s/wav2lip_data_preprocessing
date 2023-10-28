d96b10a435db407c12742952161c4a15b5a11482
90houlaoheshang <907333918@qq.com>
2023-10-28 21:21:50 +0800

    添加说明

--- 1_convert_25fps.py
+++ 1_convert_25fps.py
@@ -1,3 +1,4 @@
+# 1-run-convert-video-to-standard-25-fps
 import os
 import concurrent.futures
 import subprocess
@@ -13,17 +14,13 @@ if not os.path.exists(output_video_path):
     os.makedirs(output_video_path)
 source_dir = os.listdir(input_video_path)
 
+
 def convert_25fps(name_video):
     video = os.path.join(input_video_path, name_video)
     new_video = os.path.join(output_video_path, name_video)
     subprocess.call(f"ffmpeg -y -i {video} -filter:v fps=25 -b:v 50M {new_video}", shell=True)
 
+
 with concurrent.futures.ProcessPoolExecutor(n_processes) as executor:
     inputs = [x for x in source_dir]
     executor.map(convert_25fps, inputs)
-    
-
-
-
-
-
