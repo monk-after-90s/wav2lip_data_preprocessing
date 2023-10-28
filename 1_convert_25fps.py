4f9c01abc7c995400009f8d634269c0253b6bbf9
90houlaoheshang <907333918@qq.com>
2023-10-28 18:03:27 +0800

    Revert "更规范的命令行参数"
    This reverts commit 1ff68595870732b7aca8583a56f6b31944a5677f.

--- 1_convert_25fps.py
+++ 1_convert_25fps.py
@@ -3,21 +3,27 @@ import concurrent.futures
 import subprocess
 import sys
 
-input_video_path = sys.argv[1]
-output_video_path = sys.argv[2]
+dataset_path = sys.argv[1]
+presenter_name = sys.argv[2]
 n_processes = int(sys.argv[3])
 
+input_video_path = os.path.join(dataset_path, presenter_name, 'full_voice')
+output_video_path = os.path.join(dataset_path, presenter_name, 'full_voice_25fps')
 if not os.path.exists(output_video_path):
     os.makedirs(output_video_path)
 source_dir = os.listdir(input_video_path)
-
 
 def convert_25fps(name_video):
     video = os.path.join(input_video_path, name_video)
     new_video = os.path.join(output_video_path, name_video)
     subprocess.call(f"ffmpeg -y -i {video} -filter:v fps=25 -b:v 50M {new_video}", shell=True)
 
-
 with concurrent.futures.ProcessPoolExecutor(n_processes) as executor:
     inputs = [x for x in source_dir]
     executor.map(convert_25fps, inputs)
+    
+
+
+
+
+
